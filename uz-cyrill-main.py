65f34164d3d362fe4a19516651875f30d2a3903c
Asliddin <100646459+asliddinxanov@users.noreply.github.com>
2022-03-31 04:22:09 +0900

    update telegram bot

--- uz-cyrill-main.py
+++ uz-cyrill-main.py
@@ -6,10 +6,30 @@ Created on Wed Mar 30 11:42:26 2022
 """
 
 from transliterate import to_cyrillic, to_latin
+import telebot #pip install pyTelegramBotAPI
 
-a = input("Enter word:")
-if a.isascii():
-    print(to_cyrillic(a))
-else:
-    print(to_latin(a))
+token = "------------" #You can set parse_mode by default. HTML or MARKDOWN
+bot = telebot.TeleBot(token, parse_mode = None)
+
+@bot.message_handler(commands = ['start'])
+def send_welcome(message):
+    a = "Welcome to Transliterator"
+    a += "/nEnter word : "
+    bot.reply_to(message, a)
+
+@bot.message_handler(func = lambda message: True)
+def echo_all(message):
     
+    msg = message.text
+    a = lambda msg: to_cyrillic(msg) if msg.isascii() else to_latin(msg)
+    bot.reply_to(message, a(msg))
+    
+    # or
+    
+    # if msg.isascii():
+    #     a = to_cyrillic(msg)
+    # else:
+    #     a = to_latin(msg)
+    # bot.reply_to(message, a)
+
+bot.polling()
